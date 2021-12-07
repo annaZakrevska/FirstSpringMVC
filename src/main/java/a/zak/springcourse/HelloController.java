76c5be7bb056fea11870a60b80430c5384939d18
annaZakrevska <anutazak2@gmail.com>
2021-12-07 03:18:18 +0100

    Changes in hello-world.html

--- src/main/java/a/zak/springcourse/HelloController.java
+++ src/main/java/a/zak/springcourse/HelloController.java
@@ -3,7 +3,7 @@ package a.zak.springcourse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//Controller for first hello-word static page 
+//Controller for first hello-word static page
 @Controller
 public class HelloController {
     @GetMapping("/hello-world")
